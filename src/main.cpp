c93072666619195a1f87aad416970feba1590f71
Andreas Kamf <77665227+akamf@users.noreply.github.com>
2024-11-06 15:09:42 +0100

    utils

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,6 @@
 #include "FileManager/FileManager.h"
 #include "Utils/Logging/Logger.h"
 
-#include <iostream>
-
-// int changed = 0;
-// char filename[256] = "";
 
 int main() {
     FileManager fileManager;
